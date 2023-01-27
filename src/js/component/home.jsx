6a36ff1d5ea25cb0381c1a706b90a45730129f11
Sollb77 <qsiecuadorservicios@gmail.com>
2023-01-27 15:44:25 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-import { useState } from "react";
+//import React from 'react';
+import React,{ useState } from 'react';
 //include images into your bundle
+//create your first component
 
 
 //create your first component
 const Home = () => {
 
     
-	const [change,setChange]=useState ("");
-	
-  
+	const [sombra,setChange]=useState ("");
+
 	 
       
 	return (
-		<div className="text-center bg-dark">
-		  <div  className={`bg-danger ${(change === "red" ? "iluminado" : "")}`} 
-		  onClick={() => setChange("red")}></div>
-		  <div  className={"bg-warning" +" " +(change === "yellow" ? "iluminado" : "")} onClick={() => setChange("yellow")}></div>
-		  <div  className={`bg-success ${(change === "green" ? "iluminado" : "")}`} onClick={() => setChange("green")}></div>
-         
+
+		
+		<div className="text-center bg-dark  d-flex flex-column p-2"style={{margin: "50px auto", width: "130px"}}>
+		  
+		  
+			<div onClick={() => setChange("red")} 
+				className={"cols-3 bg-danger text-center rounded-circle m-2 p-2 "+ " " + 
+				(sombra === "red" ? "SombraR" : "")}>
+
+				</div>
+
+			<div onClick={() => setChange("yellow")} 
+				className={"cols-3 bg-warning text-center rounded-circle m-2 p-2 " + " " + 
+				(sombra === "yellow" ? "SombraY" : "")}> 
+				</div>
+			
+				<div onClick={() => setChange("green")} 
+				className={"cols-3 bg-success text-center rounded-circle m-2 p-2" + " " + 
+				(sombra === "green" ? "SombraG" : "")}> 
+				</div>
 		</div>
 
 	);
